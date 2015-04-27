d056681b531168102db52859c2696663179d1c77
Lucian Langa <lucilanga@gnome.org>
2015-04-27 20:19:22 +0200

    Revert "Backported 'yield()' hook from 1.5.x"
    This reverts commit 7595d48113dcfb39ef61d593a337143d1220ee6f.

--- hardware/arduino/cores/arduino/Arduino.h
+++ hardware/arduino/cores/arduino/Arduino.h
@@ -34,8 +34,6 @@
 extern "C"{
 #endif
 
-void yield(void);
-
 #define HIGH 0x1
 #define LOW  0x0
 
--- hardware/arduino/cores/arduino/wiring.c
+++ hardware/arduino/cores/arduino/wiring.c
@@ -111,7 +111,6 @@ void delay(unsigned long ms)
 	uint16_t start = (uint16_t)micros();
 
 	while (ms > 0) {
-		yield();
 		if (((uint16_t)micros() - start) >= 1000) {
 			ms--;
 			start += 1000;
